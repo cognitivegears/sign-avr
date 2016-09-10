33b877e041cb26274d2f32dc77c88d858f49974b
cognitivegears <techy1@gmail.com>
2016-09-10 13:08:17 -0500

    Code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,29 @@
 #define POWERTAILPIN 12
 #define REEDSWITCHPIN 11
 
-// O is one short pixels, so 119
-int numLed[] = {120, 119, 120, 120, 120};
+const int NUM_PIXELS  = 120; // 120 pixels per strip
+const int NUM_STRIPS  = 5;   // one strip per letter
+const int FULL_BRIGHT = 255; // 0-255
+const int OFF         = 0;   // 0-255
 
-Adafruit_NeoPixel strip[5] = {
-  Adafruit_NeoPixel(numLed[0],STARTPIN, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(numLed[1],STARTPIN+1, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(numLed[2],STARTPIN+2, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(numLed[3],STARTPIN+3, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(numLed[4],STARTPIN+4, NEO_GRB + NEO_KHZ800)
+// Letter O is one short pixels (oops), so we have to keep a list per letter
+int numLed[] = {NUM_PIXELS, NUM_PIXELS - 1, NUM_PIXELS, NUM_PIXELS, NUM_PIXELS};
+
+
+// We setup 5 strips, one for each letter.
+Adafruit_NeoPixel strip[NUM_STRIPS] = {
+  Adafruit_NeoPixel(numLed[0],STARTPIN, NEO_GRB + NEO_KHZ800),   // F
+  Adafruit_NeoPixel(numLed[1],STARTPIN+1, NEO_GRB + NEO_KHZ800), // O
+  Adafruit_NeoPixel(numLed[2],STARTPIN+2, NEO_GRB + NEO_KHZ800), // R
+  Adafruit_NeoPixel(numLed[3],STARTPIN+3, NEO_GRB + NEO_KHZ800), // G
+  Adafruit_NeoPixel(numLed[4],STARTPIN+4, NEO_GRB + NEO_KHZ800)  // E
 };
 
-int numStrips = sizeof(numLed) / sizeof(int);
-
 // Set up some global vars
-int redColor = 0;
-int greenColor = 0;
-int blueColor = 0;
-int letterNum = 0;
-int gearStop = 0;
+int redColor   = OFF;
+int greenColor = OFF;
+int blueColor  = OFF;
+int currentLetterNum  = 0;
 
 // Serial variables
 char cmd = NULL;
@@ -33,69 +37,51 @@ void setup()
 {
   // Start serial at 9600 baud
   Serial.begin(9600);
-  for(int i=0; i<numStrips;i++) {
+  for(int i=0; i<NUM_STRIPS;i++) {
     strip[i].begin();
-    strip[i].setBrightness(255);
+    strip[i].setBrightness(FULL_BRIGHT);
   }
-  pinMode(POWERTAILPIN, OUTPUT);
-  pinMode(REEDSWITCHPIN, INPUT_PULLUP);
-}
-void gearOn() {
- gearStop = 0;
- digitalWrite(POWERTAILPIN, HIGH);
-}
-void gearOff() {
- gearStop = 1; 
 }
 
 void setStripColor() {
-  for(int i=0; i<numLed[letterNum]; i++) {
-    strip[letterNum].setPixelColor(i, redColor,greenColor,blueColor);
+  for(int i=0; i<numLed[currentLetterNum]; i++) {
+    strip[currentLetterNum].setPixelColor(i, redColor, greenColor, blueColor);
   }
-  strip[letterNum].show();
-  strip[letterNum].show();
-  strip[letterNum].show();
-  strip[letterNum].show();
+  strip[currentLetterNum].show();
 }
 
 void doFlash(int numTimes,int myDelay) {
  for(int i=0; i<numTimes;i++) {
-   redColor = 255; greenColor = 255; blueColor = 255;
-   for(int i=0; i<numStrips;i++) {
-    letterNum = i;
-    setStripColor();  
+    allOn();
     delay(myDelay);
-   }
-   redColor = 0; greenColor = 0; blueColor = 0;
-   for(int i=0; i<numStrips;i++) {
-    letterNum = i;
-    setStripColor();  
+    allOff();
     delay(myDelay);
-   }
- }
+  }
 }
 
 void allOn() {
-   redColor = 255; greenColor = 255; blueColor = 255;
-   for(int i=0; i<numStrips;i++) {
-    letterNum = i;
+  redColor = FULL_BRIGHT; greenColor = FULL_BRIGHT; blueColor = FULL_BRIGHT;
+  for(int i=0; i<NUM_STRIPS;i++) {
+    currentLetterNum = i;
     setStripColor();  
-   }
+  }
 }
+
 void allOff() {
-   redColor = 0; greenColor = 0; blueColor = 0;
-   for(int i=0; i<numStrips;i++) {
-    letterNum = i;
+  redColor = OFF; greenColor = OFF; blueColor = OFF;
+  for(int i=0; i<NUM_STRIPS;i++) {
+    currentLetterNum = i;
     setStripColor();  
-   }
+  }
 }
 
 void doMarquee() {
-  redColor = 255;
-  greenColor = 255;
-  blueColor = 255;
-  for(int i=0; i<numStrips;i++) {
-   letterNum = i;
+  redColor = FULL_BRIGHT;
+  greenColor = FULL_BRIGHT;
+  blueColor = FULL_BRIGHT;
+
+  for(int i=0; i<NUM_STRIPS;i++) {
+   currentLetterNum = i;
    setStripColor();  
    delay(1000);
   }
@@ -107,68 +93,63 @@ void runSpecial(int valType) {
   {
    case '00': doMarquee();break;
    case '01': doFlash(10,500); break;
-   case '02': gearOn();break;
-   case '03': gearOff();break;
-   case '04': break;
+   case '02': allOn(); break;
+   case '03': allOff(); break;
   }
- 
-  for(int i=0; i<numLed[letterNum]; i++) {
-    strip[letterNum].setPixelColor(i, redColor,greenColor,blueColor);
+
+  // Reset lights on current letter
+  for(int i=0; i<numLed[currentLetterNum]; i++) {
+    strip[currentLetterNum].setPixelColor(i, redColor, greenColor, blueColor);
   }
-  strip[letterNum].show();
+  strip[currentLetterNum].show();
 }
 
 
 
 void loop()
 {
-// allOn();
-// delay(1000);
-// allOff();
-// delay(1000);
-// return;
-  if(gearStop == 1) {
-   if(digitalRead(REEDSWITCHPIN) == LOW) {
-     digitalWrite(POWERTAILPIN, LOW);
-  } 
-}
-else {
-gearOn();
-}
- 
   if(!Serial.available()) {
     return;
   }
- char readChar = Serial.read(); 
- if (readChar == ';') {
-  firstChar = NULL;
-  cmd = NULL;
-  return;
- }
 
- if (!cmd) {
-  cmd = readChar;
- }
- else {
-   if (!firstChar) {
-    firstChar = readChar;
-   }
-   else {
-    char mybuf [3];
-    sprintf(mybuf,"%c%c",firstChar,readChar);
-    int value = strtol(mybuf,NULL,16);
-    switch(cmd)
-   {
-    case 'r': redColor = value; setStripColor();break;
-    case 'g': greenColor = value;  setStripColor();break;
-    case 'b': blueColor = value;  setStripColor();break;
-    case 'l': letterNum = value;  break;
-    case 's': runSpecial(value); break;
-//    case 's': gearOff(); break;
-   }
-   cmd = NULL;
-   firstChar = NULL;
-   }
- }
+  char readChar = Serial.read(); 
+
+  // Reset whenever an end of command character ";" is read
+  if (readChar == ';') {
+    firstChar = NULL;
+    cmd = NULL;
+    return;
+  }
+
+  // First read a command, then a value.  If a command hasn't been read yet, 
+  // then read it and store for next iteration of the loop.
+  if (!cmd) {
+    cmd = readChar;
+  }
+  else {
+    // values are two characters, read the first character and store for next
+    // iteration of the loop if one hasn't already been read
+    if (!firstChar) {
+      firstChar = readChar;
+    }
+    else {
+      char mybuf [3];
+      sprintf(mybuf,"%c%c",firstChar,readChar);
+      int value = strtol(mybuf,NULL,16);
+
+      // After getting the command and value, determine what to execute
+      switch(cmd)
+      {
+        case 'r': redColor = value; setStripColor();break;
+        case 'g': greenColor = value;  setStripColor();break;
+        case 'b': blueColor = value;  setStripColor();break;
+        case 'l': currentLetterNum = value;  break;
+        case 's': runSpecial(value); break;
+      }
+      // After running command, clear it before next iteration
+      cmd = NULL;
+      firstChar = NULL;
+    }
+  }
 }
 
